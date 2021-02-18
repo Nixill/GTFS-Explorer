15672bdc21c74d31dd26a8dfff1f67eb6cf16abb
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-02-17 19:14:06 -0500

    Added main Pages and style

---
+++ GTFS-Explorer.FrontEnd/Pages/MainPages/SystemMap.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GTFS_Explorer.FrontEnd.Pages.MainPages
+{
+    public class SystemMapModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
