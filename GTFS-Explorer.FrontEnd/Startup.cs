ba23f38f16f1694773e20f236dbfa9deaed7b72c
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-02-20 20:57:18 -0500

    Removed runtime compilation, didn't work with electron

--- GTFS-Explorer.FrontEnd/Startup.cs
+++ GTFS-Explorer.FrontEnd/Startup.cs
@@ -25,7 +25,7 @@ namespace GTFS_Explorer.FrontEnd
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages().AddRazorRuntimeCompilation();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
