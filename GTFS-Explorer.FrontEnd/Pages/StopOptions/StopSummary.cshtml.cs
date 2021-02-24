13342e004a5e204b7c36c080a33369bbd455707e
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-02-24 16:46:11 -0500

    Added Stop Options

---
+++ GTFS-Explorer.FrontEnd/Pages/StopOptions/StopSummary.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace GTFS_Explorer.FrontEnd.Pages.StopOptions
+{
+    public class StopSummaryModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
