3d983fdd9715ec24551ff876d11b6d3cd190205b
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-04-07 02:58:04 -0400

    Added Feed Info to selection page & URL click event

--- src/GTFS-Explorer.FrontEnd/Pages/MainPages/Selection.cshtml.cs
+++ src/GTFS-Explorer.FrontEnd/Pages/MainPages/Selection.cshtml.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using ElectronNET.API;
+using GTFS.Entities;
+using GTFS_Explorer.BackEnd.Readers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text.RegularExpressions;
 
 namespace GTFS_Explorer.FrontEnd.Pages.MainPages
 {
     public class SelectionModel : PageModel
     {
-        public void OnGet()
-        {
+        private readonly GTFSFeedReader _reader;
 
-        }
+		public SelectionModel(GTFSFeedReader reader)
+		{
+			_reader = reader;
+		}
+
+		[BindProperty]
+		public FeedInfo FeedInfo { get; set; }
+
+		public void OnGet()
+        {
+			FeedInfo = _reader.Feed.GetFeedInfo();
+
+			if(!string.IsNullOrEmpty(FeedInfo.StartDate) &&
+			   !string.IsNullOrEmpty(FeedInfo.EndDate))
+			{
+				FeedInfo.StartDate = 
+					Regex.Replace(FeedInfo.StartDate, @"^(....)(..)(..)$", "$1-$2-$3");
+				FeedInfo.EndDate = 
+					Regex.Replace(FeedInfo.EndDate, @"^(....)(..)(..)$", "$1-$2-$3");
+			}
+
+			Electron.IpcMain.On("open-publisher-url", async (args) =>
+			{
+				await Electron.Shell.OpenExternalAsync(FeedInfo.PublisherUrl);
+			});
+		}
     }
 }
