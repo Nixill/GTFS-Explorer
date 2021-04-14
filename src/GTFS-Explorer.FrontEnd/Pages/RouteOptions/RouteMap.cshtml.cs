94c37abeab612c61b062e81ff80ad20cf52f3621
Edgar Terrazas <Ragdety@users.noreply.github.com>
2021-04-14 14:02:17 -0400

    Added Map view to RouteMap

--- src/GTFS-Explorer.FrontEnd/Pages/RouteOptions/RouteMap.cshtml.cs
+++ src/GTFS-Explorer.FrontEnd/Pages/RouteOptions/RouteMap.cshtml.cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using GTFS.Entities;
+using GTFS_Explorer.Core.Interfaces;
 using GTFS_Explorer.Core.Interfaces.RepoInterfaces;
+using GTFS_Explorer.Core.Models.Structs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -12,19 +13,25 @@ namespace GTFS_Explorer.FrontEnd.Pages.RouteOptions
     public class RouteMapModel : PageModel
     {
         private readonly IRoutesRepository _routesRepository;
+        private readonly IRouteMapBuilderService _routeMapBuilderService;
 
-		public RouteMapModel(IRoutesRepository routesRepository)
+		public RouteMapModel(
+            IRoutesRepository routesRepository, 
+            IRouteMapBuilderService routeMapBuilderService)
 		{
 			_routesRepository = routesRepository;
+			_routeMapBuilderService = routeMapBuilderService;
 		}
 
-        public string RouteId { get; set; }
+		public string RouteId { get; set; }
         public Route Route { get; set; }
+		public List<List<Coordinate>> RouteShapes { get; set; }
 
-        public void OnGet(string routeId)
+		public void OnGet(string routeId)
         {
             RouteId = routeId;
             Route = _routesRepository.GetRouteById(RouteId);
+            RouteShapes = _routeMapBuilderService.GetShapes(routeId);
         }
     }
 }
